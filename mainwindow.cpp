12703ea7fa46484766cd8b7e3b0b8da8e44ec544
RPlsicik <rplsicik@gmail.com>
2021-11-19 20:39:10 +0100

    Shorten informat about COMports

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -24,6 +24,7 @@ MainWindow::MainWindow(QWidget *parent)
 
     connect(port1,&QSerialPort::readyRead,this,&MainWindow::port1_RX);
     connect(port2,&QSerialPort::readyRead,this,&MainWindow::port2_RX);
+
 }
 
 MainWindow::~MainWindow()
@@ -50,13 +51,13 @@ void MainWindow::on_pushButton_clicked()
     ui->textBrowser->clear();
     const auto infos = QSerialPortInfo::availablePorts();
     for (const QSerialPortInfo &info : infos) {
-        QString s = QObject::tr("Port: ") + info.portName() + "\n"
-                    + QObject::tr("Location: ") + info.systemLocation() + "\n"
-                    + QObject::tr("Description: ") + info.description() + "\n"
-                    + QObject::tr("Manufacturer: ") + info.manufacturer() + "\n"
-                    + QObject::tr("Serial number: ") + info.serialNumber() + "\n"
-                    + QObject::tr("Vendor Identifier: ") + (info.hasVendorIdentifier() ? QString::number(info.vendorIdentifier(), 16) : QString()) + "\n"
-                    + QObject::tr("Product Identifier: ") + (info.hasProductIdentifier() ? QString::number(info.productIdentifier(), 16) : QString()) + "\n";
+        QString s = QObject::tr("Port: ") + info.portName();// + "\n";
+                   // + QObject::tr("Location: ") + info.systemLocation() + "\n"
+                   // + QObject::tr("Description: ") + info.description() + "\n"
+                   // + QObject::tr("Manufacturer: ") + info.manufacturer() + "\n"
+                   // + QObject::tr("Serial number: ") + info.serialNumber() + "\n"
+                   //+ QObject::tr("Vendor Identifier: ") + (info.hasVendorIdentifier() ? QString::number(info.vendorIdentifier(), 16) : QString()) + "\n"
+                   // + QObject::tr("Product Identifier: ") + (info.hasProductIdentifier() ? QString::number(info.productIdentifier(), 16) : QString()) + "\n";
                    // + QObject::tr("Busy: ") + (info.isBusy() ? QObject::tr("Yes") : QObject::tr("No")) + "\n";
 
         //auto label = new QLabel(s);
